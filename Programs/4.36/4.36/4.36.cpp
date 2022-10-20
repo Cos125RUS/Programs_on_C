70b644d57c0572de767d7524f98651cec5103949
Cos125RUS <val250640@mail.ru>
2022-10-20 17:57:21 +1000

    4.36   Печать массива (рекурсия)

---
+++ Programs/4.36/4.36/4.36.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <locale>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+
+void creature(int[], int);
+
+void print(int[], int);
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    srand(time(0));
+
+    const int size = 10;
+
+    int array[size] = { 0 };
+
+
+    creature(array, size);
+    print(array, size);
+
+
+
+    cout << "\n\n\n\n\n\n\n\n\n\n";
+
+    return 0;
+}
+
+void creature(int array[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = 1 + rand() % 999;
+    }
+}
+
+void print(int arr[], int size)
+{
+    if (size > 0)
+    {
+        cout << arr[size - 1] << ' ';
+        print(arr, size - 1);
+    }
+}
