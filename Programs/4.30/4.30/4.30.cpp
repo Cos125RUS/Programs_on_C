e387c3d5039f689a5f693f4de96e1239c5679018
Cos125RUS <val250640@mail.ru>
2022-10-20 11:25:45 +1000

    Complete

--- Programs/4.30/4.30/4.30.cpp
+++ Programs/4.30/4.30/4.30.cpp
@@ -12,9 +12,15 @@ const int line = 10, n = 100;
 
 int block[line][n] = { 0 };
 
-void creature(int[], int);
+void creature(int[n], int);
 
 void bucket_sort(int[n]);
+
+void assembling(int[n]);
+
+void print(int[n]);
+
+void zero();
 
 int main()
 {
@@ -28,13 +34,8 @@ int main()
 
     creature(array, n);
     bucket_sort(array);
-
-    for (int i = 0; i < n; i++)
-    {
-        if (i % 10 == 0 && i != 0)
-            cout << endl;
-        cout << setw(5) << array[i];
-    }
+    // print(array);
+    
 
 
 
@@ -51,46 +52,73 @@ int main()
 void creature(int array[n], int size)
 {
     for (int i = 0; i < size; i++)
-        array[i] = 1 + rand() % 1000;
+    {
+        array[i] = 1 + rand() % 999;        
+    }
+
+    print(array);
 }
 
 void bucket_sort(int array[n])
 {
     int index;
 
-    for (int i = n; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         index = array[i] % 10;
         block[index][i] = array[i];
     }
 
-    for (int k = 0; k < n; k++)
-        for (int j = 0; j < line; j++)
-            for (int i = 0; i < n; i++)
-                if (block[j][i] > 0)
-                    array[k] = block[j][k];
-
-    for (int i = n; i < n; i++)
+    assembling(array);
+    print(array);
+    zero();
+                
+    for (int i = 0; i < n; i++)
     {        
-        index = array[i] / 10 % 10;
+        index = (array[i] / 10) % 10;
         block[index][i] = array[i];
     }
+    
+    assembling(array);
+    print(array);
+    zero();
 
-    for (int k = 0; k < n; k++)
-        for (int j = 0; j < line; j++)
-            for (int i = 0; i < n; i++)
-                if (block[j][i] > 0)
-                    array[k] = block[j][k];
-
-    for (int i = n; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        index = array[i] / 100 % 10;
+        index = (array[i] / 100) % 10;
         block[index][i] = array[i];
     }
+    
+    assembling(array);
+    print(array);
+}
 
-    for (int k = 0; k < n; k++)
-        for (int j = 0; j < line; j++)
-            for (int i = 0; i < n; i++)
-                if (block[j][i] > 0)
-                    array[k] = block[j][k];
+void assembling(int array[n])
+{
+    int k = 0;
+    for (int i = 0; i < line; i++)
+        for (int j = 0; j < n; j++)
+            if (block[i][j] > 0)
+            {
+                array[k] = block[i][j];
+                k++;
+            }
+}
+
+void print(int array[n])
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (i % 10 == 0 && i != 0)
+            cout << endl;
+        cout << setw(5) << array[i];
+    }
+    cout << endl << endl;
+}
+
+void zero()
+{
+    for (int i = 0; i < line; i++)
+        for (int j = 0; j < n; j++)
+            block[i][j] = 0;
 }
