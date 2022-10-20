0ea5b084bdd90b82cccb7faddfdc842329748a8e
Cos125RUS <val250640@mail.ru>
2022-10-20 16:57:55 +1000

    4.33

---
+++ Programs/4.32/4.32/4.32.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <locale>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+bool test_polindrome(char[], int, int);
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    char text[5] = { 'r','a','d','a','r' };
+    
+          
+    cout << test_polindrome(text, 5, 0);
+    
+
+    char txt;
+    cout << "Enter text: ";
+    cin >> txt;
+    cout << "\n\ntxt: " << txt[2];
+
+
+
+    cout << "\n\n\n\n\n\n\n\n\n\n";
+
+    return 0;
+}
+
+bool test_polindrome(char text[], int size, int a)
+{
+    if (a == (size / 2))
+        return true;
+    else
+    {
+        if (text[a] == text[size - 1 - a]) return test_polindrome(text, size, a + 1);
+        else return false;        
+    }
+}
---
+++ Programs/4.33/4.33/4.33.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <locale>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void creature(int[], int);
+
+int linear_seach(int[], int, int);
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    const int array_size = 100;
+
+    int arr[array_size], seach_key, element;
+
+    creature(arr, array_size);
+
+    cout << "Enter seachh key: ";
+    cin >> seach_key;
+
+    element = linear_seach(arr, array_size, seach_key);
+
+    if (element != -1)
+        cout << "\nSeach key found in the " << element << " element of array.";
+    else
+        cout << "\nSeach key not found in array.";
+
+
+
+
+
+    cout << "\n\n\n\n\n\n\n\n\n\n";
+
+    return 0;
+}
+
+int linear_seach(int arr[], int size, int key)
+{
+    if (size == 0) return -1;
+    else
+    {
+        if (arr[size - 1] == key) return size - 1;
+        else return linear_seach(arr, size - 1, key);
+    }
+}
+
+void creature(int array[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = 1 + rand() % 99;
+    }
+}
