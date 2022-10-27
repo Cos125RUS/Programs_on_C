28d077fe1e31d030c9d245861ad193d301feb729
Cos125RUS <val250640@mail.ru>
2022-10-27 14:39:23 +1000

    5.5 Использование const с указателями

--- Programs/5 Указатели/Введение/5.5/5.5/5.5.cpp
+++ Programs/5 Указатели/Введение/5.5/5.5/5.5.cpp
@@ -4,25 +4,58 @@
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
+#include <cctype>
 
 using namespace std;
+
+void convertToUppercase(char*);
+
+void printCharacters(const char*);
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
+    char string1[] = "characters and $32.98";
+
+    cout << "Строка до преобразования:   " << string1;
+    convertToUppercase(string1);
+    cout << "\nСтрока после преобразования:  " << string1 << endl;
+
+         
+    char string2[] = "Печатль символьнос строки";
+
+    cout << "Строка:\n";
+    printCharacters(string2);
+    cout << endl;
 
 
+    int x, y;
 
+    int* const ptr = &x;
 
+    *ptr = 7;
+    // ptr = &y;
 
-
-
-
-
+    cout << "ptr = " << ptr << "\n*ptr = " << *ptr << "\nx = " << x;
 
 
     cout << "\n\n\n\n\n\n\n\n\n\n";
 
     return 0;
+}
+
+void convertToUppercase(char *sPtr)
+{
+    while (*sPtr != '\0')
+    {
+        if (islower(*sPtr)) *sPtr = toupper(*sPtr);
+        ++sPtr;
+    }
+}
+
+void printCharacters(const char *sPtr)
+{
+    for (; *sPtr != '\0'; sPtr++)
+        cout << *sPtr;
 }
