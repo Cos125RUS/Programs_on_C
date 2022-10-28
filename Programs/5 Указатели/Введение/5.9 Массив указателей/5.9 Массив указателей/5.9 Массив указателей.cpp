7082087bd6758688fcc039a765bc3cfd78d52e51
Cos125RUS <val250640@mail.ru>
2022-10-28 15:26:40 +1000

    5.10 Тасование и сдача карт

---
+++ Programs/5 Указатели/Введение/5.9 Массив указателей/5.9 Массив указателей/5.9 Массив указателей.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <locale>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+
+    const char *suit[4] = { "Черви", "Бубны", "Трефы", "Пики" };
+
+    cout << suit[2];
+
+
+
+    cout << "\n\n\n\n\n\n\n\n\n\n";
+
+    return 0;
+}
