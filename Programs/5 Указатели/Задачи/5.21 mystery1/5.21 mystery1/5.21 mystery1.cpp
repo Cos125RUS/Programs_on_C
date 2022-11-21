db656a8bd62a2b14446153f174635e06faeb74b5
Cos125RUS <val250640@mail.ru>
2022-11-21 12:48:36 +1000

    5.20 - 5.24

---
+++ Programs/5 Указатели/Задачи/5.21 mystery1/5.21 mystery1/5.21 mystery1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <locale>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void mystery1(char*, const char*);
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    char string1[80], string2[80];
+
+    cout << "Enter two strings: ";
+    cin >> string1 >> string2;
+    mystery1(string1, string2);
+    cout << string1 << endl;
+
+
+
+
+
+
+
+
+    cout << "\n\n\n\n\n\n\n\n\n\n";
+
+    return 0;
+}
+
+void mystery1(char* s1, const char* s2)
+{
+    while (*s1 != '\0')
+        ++s1;
+
+    for (; *s1 = *s2; s1++, s2++);
+}
