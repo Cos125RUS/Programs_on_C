e8faee7ac89db551698c8c92731ce29608df935a
Cos125RUS <val250640@mail.ru>
2022-12-02 10:32:00 +1000

    5.35 Кодирование фраз

---
+++ Programs/5 Указатели/Задачи/5.34 Лимерики/5.34 Лимерики/5.34 Лимерики.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+#include <locale>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstddef>
+#include <string.h>
+#include <errno.h>
+
+using namespace std;
+
+void generate(char[][100], const char* [], const char* [], const char* [], const char* [], int*);
+void createName(char [], const char* [], const char* [], bool);
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	srand(time(0));
+
+	const char* name_male[5] = { "Ген", "Вов", "Вас", "Пет", "Ван" };
+	const char* name_female[5] = { "Юл", "Тан", "Кат", "Маш", "Даш" };
+	const char* name_endings[7] = { "у ", "е ", "ы ", "и ", "ю ", "а ", "я " };
+
+	const char* pronouns_nominative[4] = { "он ", "она ", "они ", "оно " };
+	const char* pronouns_object[6] = { "его ", "ему ", "ей ", "её ", "их ", "им " };
+
+	const char* object_male[5] = { "бокал", "рюкзак", "стул", "телефон", "ключ" };
+	const char* object_female[5] = { "подушк", "книг", "ручк", "тарелк", "салфетк" };
+	const char* object_neuter_singular[4] = { "одеял", "блюдц", "дел", "слов" };
+	const char* object_only_plural[5] = { "часы ", "деньги ", "сутки ", "очки ", "выборы " };
+	const char* object_endings[5] = { "ы ", "и ", "ья ", "и ", "а " };
+
+	const char* verb[5] = { "дал ", "взял ", "попросил ", "выбрал ", "использовал " };
+
+	const char* adverb[5] = { "много ", "мало ", "ещё ", "чуть ", "совсем " };
+
+	const char* adjective[5] = { "нов", "больш", "последн", "хорош", "маленьк" };
+	const char* adjective_endings_male[3] = { "ый ", "ой ", "ий " };
+	const char* adjective_endings_female[2] = { "ая ", "яя " };
+	const char* adjective_endings_plural[2] = { "ые ", "ие "};
+
+	const char* preposition[6] = { "на ", "в ", "из ", "под ", "к ", "у " };
+	
+	char d[20][100];
+
+	bool male = rand() % 2;
+	char name[20];
+
+	if (male) createName(name, name_male, name_endings, male);
+	else createName(name, name_female, name_endings, male);
+
+	cout << name;
+
+
+
+
+
+	cout << "\n\n\n\n\n\n\n\n\n\n";
+
+	return 0;
+}
+
+void generate(char crossword[][100], const char* name[], const char* noun[], const char* verb[], const char* adjective[], int* row)
+{
+	
+}
+
+void createName(char name[], const char* root[], const char* ending[], bool male)
+{
+	int i = rand() % 5;
+
+	strcpy_s(name, 5, root[i]);
+	
+	if (male)
+		if (i < 2)
+			strcat_s(name, 5, ending[5]);
+		else
+			strcat_s(name, 5, ending[6]);
+	else
+		if (i < 3)
+			strcat_s(name, 5, ending[6]);
+		else
+			strcat_s(name, 5, ending[5]);
+}
---
+++ Programs/5 Указатели/Задачи/5.35/5.35/5.35.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <locale>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstddef>
+#include <string.h>
+#include <errno.h>
+
+using namespace std;
+
+int separation(char text[200], char words[][20]);
+void coding(char words[][20], const int* size);
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    srand(time(0));
+
+    char text[200], words[10][20];
+    int size;
+
+    cin.getline(text, 200, '\n');
+    cout << endl << endl;
+
+    size = separation(text, words);
+    coding(words, &size);
+
+    
+
+    
+
+
+
+    cout << "\n\n\n\n\n\n\n\n\n\n";
+
+    return 0;
+}
+
+int separation(char text[200], char words[][20])
+{
+    char* token;
+    char* txt;
+    int i = 0;
+    
+    token = strtok_s(text, " ", &txt);
+
+    while (token != NULL)
+    {
+		strcpy_s(words[i], token);
+		i++;
+		token = strtok_s(NULL, " ", &txt);
+    }
+
+    return i;
+}
+
+void coding(char words[][20], const int* size)
+{
+    int length;
+    char firstLetter;
+    char* others;
+    const char* increase[5] = { "ay", "df", "gh", "jk", "vb" };
+    char code[200] = "";
+
+    for (int i = 0; i < *size; i++)
+    {
+        length = strlen(words[i]);
+
+        if (length > 1)
+        {
+			firstLetter = words[i][0];
+			others = &words[i][1];
+			strncpy_s(words[i], others, length - 1);
+            words[i][length - 1] = firstLetter;
+            words[i][length] = '\0';
+            strcat_s(words[i], increase[rand()%5]);
+        }
+
+        if (i != (*size - 1))
+			strcat_s(words[i], " ");
+
+        strcat_s(code, words[i]);
+    }
+
+    cout << code;
+}
