270acf30526b882d6d10da13c2adaee2f6982138
Cos125RUS <val250640@mail.ru>
2022-12-02 16:20:03 +1000

    5.37 Текст задом наперёд

---
+++ Programs/5 Указатели/Задачи/5.37 Лексемы задом наперёд/5.37 Лексемы задом наперёд/5.37 Лексемы задом наперёд.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <locale>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstddef>
+#include <string.h>
+#include <errno.h>
+
+using namespace std;
+
+int separation(char text[200], char words[10][20]);
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    char text[200];
+    char words[10][20];
+
+    cin.getline(text, 200, '\n');
+
+    int size = separation(text, words);
+
+    for (int i = size - 1; i >= 0; i--)
+    {
+        cout << words[i] << ' ';
+    }
+
+
+    cout << "\n\n\n\n\n\n\n\n\n\n";
+
+    return 0;
+}
+
+int separation(char text[200], char words[10][20])
+{
+    char* txt;
+    char* token;
+    int i = 0;
+
+    token = strtok_s(text, " ", &txt);
+
+    while (token != NULL)
+    {
+        strcpy_s(words[i], token);
+        i++;
+        token = strtok_s(NULL, " ", &txt);
+    }
+
+    return i;
+}
