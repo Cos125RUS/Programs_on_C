6e30443fba9deab1d99816962b568588ab45fb26
Cos125RUS <val250640@mail.ru>
2022-12-05 15:08:40 +1000

    5.41 Длина строки

---
+++ Programs/5 Указатели/Задачи/5.41 Длина строки/5.41 Длина строки/5.41 Длина строки.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <locale>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstddef>
+#include <string.h>
+#include <errno.h>
+
+using namespace std;
+
+int stringLength1(const char* s) 
+{ 
+    int i = 0;
+    for (i; s[i] != '\0'; i++);
+    return i; 
+};
+
+int stringLength2(const char* s)
+{
+    int i = 0;
+    while (*(s+i) != '\0') i++;
+    return i;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    const char string1[] = "translate_google ";
+    const char string2[] = "translate.yandex.ru ";
+
+    cout << stringLength1(string1) << endl;
+    cout << stringLength2(string2) << endl;
+
+
+
+    cout << "\n\n\n\n\n\n\n\n\n\n";
+
+    return 0;
+}
