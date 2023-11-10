e5da2353463c1b8bc70f68c84ef5497fc34a486c
Cos125RUS <val250640@mail.ru>
2023-11-10 14:54:06 +1000

    11.6,11.7,11.11

---
+++ Programs/11.Streams(in-out)/Exercises/11.11/11.11/11.11.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <locale>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstddef>
+#include <cassert>
+#include <string.h>
+#include <errno.h>
+#include <ctype.h>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    char buffer[100];
+    int size = 0;
+    char c;
+    while ((c = cin.get()) != '\n' && size < 100) {
+        buffer[size++] = c;
+    }
+
+    cout.setf(ios::adjustfield);
+    cout.fill('*');
+    cout.width(size * 2);
+    cout << setw(size / 2) << "";
+    cout.write(buffer, size);
+    cout << setw(size / 2) << "";
+
+
+
+
+
+
+
+
+    cout << "\n\n\n\n\n\n\n\n\n\n";
+
+    return 0;
+}
---
+++ Programs/11.Streams(in-out)/Exercises/11.7/11.7/11.7.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <locale>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstddef>
+#include <cassert>
+#include <string.h>
+#include <errno.h>
+#include <ctype.h>
+#include <cstring>
+
+using namespace std;
+
+void print(int x) {
+    cout << dec << x << endl;
+    cout << oct << x << endl;
+    cout << hex << x << endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int x = 0;
+    char buffer[20];
+    char c;
+    int i = 0;
+    cout << "Enter: ";
+    while ((c = cin.get()) != '\n' && i < 20) {
+        buffer[i++] = c;
+    }
+    //cin.getline(buffer, 20, '\n');
+    cout << setiosflags(ios::showbase);
+    if (buffer[0] == '0') {
+        if (buffer[1] == 'x') {
+            for (int j = i - 1, k = 0; j > 1; j--, k++) {
+                switch (buffer[j])
+                {
+                case 'a':
+                    x += 10 * pow(16, k);
+
+                    break;
+                case 'b':
+                    x += 11 * pow(16, k);
+                    break;
+                case 'c':
+                    x += 12 * pow(16, k);
+                    break;
+                case 'd':
+                    x += 13 * pow(16, k);
+                    break;
+                case 'e':
+                    x += 14 * pow(16, k);
+                    break;
+                case 'f':
+                    x += 15 * pow(16, k);
+                    break;
+                default:
+                    x += (buffer[j] - '0') * pow(16, k);
+                    break;
+                }
+            }
+            print(x);
+        }
+        else {
+            for (int j = i - 1, k = 0; j > 0; j--, k++) {
+                x += (buffer[j] - '0') * pow(8, k);
+            }
+            print(x);
+        }
+    }
+    else {
+        for (int j = i-1, k = 0; j >= 0; j--, k++) {
+            x += (buffer[j] - '0') * pow(10, k);
+        }
+        print(x);
+    }
+
+
+
+
+
+
+
+
+    cout << "\n\n\n\n\n\n\n\n\n\n";
+}
