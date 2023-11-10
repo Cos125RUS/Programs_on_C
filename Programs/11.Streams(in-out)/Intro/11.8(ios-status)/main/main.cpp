1f4ee5939f5015dfbfa0734034814b41654027ef
Cos125RUS <val250640@mail.ru>
2023-11-10 11:15:28 +1000

    AllIntro

---
+++ Programs/11.Streams(in-out)/Intro/11.8(ios-status)/main/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int x;
+    cout << cin.rdstate() << endl << cin.eof() << endl << cin.fail() << endl << cin.bad() << endl << cin.good() << endl << endl;
+    cin >> x;
+    cout << cin.rdstate() << endl << cin.eof() << endl << cin.fail() << endl << cin.bad() << endl << cin.good() << endl << endl;
+    cin.clear();
+    cout << cin.fail() << endl << cin.good() << endl << endl;
+
+
+
+
+
+
+
+    cout << "\n\n\n\n\n\n\n\n\n\n";
+}
